6854ec3eb9a66ba2390063eef5387dcbd3657db2
RayaneVersiani <76855528+RayaneRoque@users.noreply.github.com>
2022-09-17 00:15:45 -0300

    Initial commit

---
+++ run.py
@@ -0,0 +1,11 @@
+#Como mudar as cores do seu código com metodo ANSI \033[m
+print('\033[40m COR TEXTO BRANCO\033[m'), print('\033[7:40m COR FUNDO BRANCO\033[m')
+print('\033[31m COR TEXTO VERMELHO\033[m'), print('\033[41m COR FUNDO VERMELHO\033[m')
+print('\033[32m COR TEXTO VERDE\033[m'), print('\033[42m COR FUNDO VERDE\033[m')
+print('\033[33m COR TEXTO AMARELO\033[m'), print('\033[43m COR FUNDO AMARELO\033[m')
+print('\033[34m COR TEXTO AZUL\033[m'), print('\033[44m COR FUNDO AZUL\033[m')
+print('\033[35m COR TEXTO ROXO\033[m'), print('\033[45m COR FUNDO ROXO\033[m')
+print('\033[36m COR TEXTO CIANO\033[m'), print('\033[46m COR FUNDO CIANO\033[m')
+print('\033[37m COR TEXTO CINZA\033[m'), print('\033[47m COR FUNDO CINZA\033[m')
+
+
