0ef8e279ff6cfa7a3f4dd7f3cde2dba95e1af5d1
RayaneVersiani <76855528+RayaneRoque@users.noreply.github.com>
2022-09-18 22:54:51 -0300

    v

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-print('\033[36m'*20)
-print('-'*20)
+print('\033[36m*' * 40)
+print('-' * 40)
 jogador = ['Paulo', 'Mario', 'José', 'Marcos']
 lista = list()
 for f in jogador:
     lista.append(f)
 print(lista)
+print('-' * 40)
